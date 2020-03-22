e23be2a9e0e90ceab30189209d7795ddaca25d3d
Stenio Oliveira <steniooliv@gmail.com>
2020-03-22 11:34:21 -0300

    samples of operators

---
+++ operator/sample-01.php
@@ -0,0 +1,13 @@
+<?php
+
+//attrib
+$name = "Stenio";
+
+//concat
+echo $name . " more text<br>";
+
+//compost
+$name .= " trainer";
+echo $name;
+
+?>
---
+++ operator/sample-02.php
@@ -0,0 +1,13 @@
+<?php
+
+//sum
+$totalValue = 0;
+$totalValue += 100;
+$totalValue += 25;
+
+echo $totalValue;
+
+//subtraction
+
+
+?>
