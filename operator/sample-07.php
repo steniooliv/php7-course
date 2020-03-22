9ee577811e1d034ba152317c3f626f6aed38f14b
Stenio Oliveira <steniooliv@gmail.com>
2020-03-22 14:31:22 -0300

    increment operator

---
+++ operator/sample-07.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 10;
+
+//incremental operator
+echo $a++;
+
+echo "<br>";
+
+echo $a;
+
+echo "<br>";
+
+echo ++$a;
+
+echo "<br>";
+
+echo $a--;
+
+echo "<br>";
+
+echo $a;
+
+echo "<br>";
+
+echo --$a;
+
+?>
