fd24fff0047d80ed1b3b0c8b95ccf2806728de1a
Stenio Oliveira <steniooliv@gmail.com>
2020-03-22 14:22:47 -0300

    booleand operators

---
+++ operator/sample-04.php
@@ -0,0 +1,24 @@
+<?php
+
+//operator boolean
+$a = 30;
+$b = 55;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+var_dump($a == $b);     //only value
+var_dump($a === $b);    //value and type
+
+echo "<br>";
+
+var_dump($a != $b);     //only value
+var_dump($a !== $b);    //value and type
+echo "<br>";
+
+?>
