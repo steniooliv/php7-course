bd3355aafd5fcd4a850ec447e72fefdc63179ba2
Stenio Oliveira <steniooliv@gmail.com>
2020-03-22 14:36:25 -0300

    logic operator

---
+++ operator/sample-08.php
@@ -0,0 +1,15 @@
+<?php
+
+$result = (10 + 3) / 2;
+
+echo $result;
+echo "<br>";
+
+//logic operator &&(and) ||(or)
+var_dump($result > 5 && 10 + 5 < 20);
+
+echo "<br>";
+
+var_dump($result > 5 || 10 + 5 < 10);
+
+?>
