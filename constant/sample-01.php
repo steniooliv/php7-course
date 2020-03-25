d7f861f0353519f65e66f8547807a773e291f06c
Stenio Oliveira <steniooliv@gmail.com>
2020-03-24 22:55:52 -0300

    constant samples

---
+++ constant/sample-01.php
@@ -0,0 +1,7 @@
+<?php
+
+define("SERVER","127.0.0.1");
+
+echo SERVER;
+
+?>
---
+++ constant/sample-02.php
@@ -0,0 +1,14 @@
+<?php
+
+//constant with array
+
+define("DB", [
+    "127.0.0.1",
+    "root",
+    "password",
+    "test"
+]);
+
+print_r(DB);
+
+?>
